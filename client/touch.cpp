5fbee443b07598ec793217b8d45f24b1ae5f7ae2
Caleb <caleb.schoepp@gmail.com>
2019-03-09 00:02:42 -0700

    Start work on dfs solving algo

---
+++ client/touch.cpp
@@ -0,0 +1,8 @@
+/*
+
+This code will take in touch input.
+
+Based on the state of the client when this is called, it will look in different areas and return
+different things from the touchscreen.
+
+*/
---
+++ server/algo_dfs.h
@@ -0,0 +1,10 @@
+#ifndef ALGO_DFS_H
+#define ALGO_DFS_H
+
+class Dfs {
+public:
+    void solve();
+    bool check_solvability();
+};
+
+#endif

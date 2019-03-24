6c57d31084e2aa7d38aa9ad2410f925d43955805
Caleb Schoepp <caleb.schoepp@gmail.com>
2019-03-23 20:00:56 -0600

    Thoughts

---
+++ server/server.cpp
@@ -0,0 +1,36 @@
+// includes
+
+using namespace std;
+
+int main() {
+    // Simple state machine based on initial input from serial
+    // Timeouts bring back to main function
+  
+    // State variables
+    // int ...
+    // bool ....
+  
+    /*
+    string state;
+    state = readInState();
+
+    switch (state) {
+      case a:
+
+      case b:
+
+      etc.
+
+      }
+
+
+each case will call a function that then reads in the rest of the input etc.
+
+so it  would be like server is s and client is c
+
+c: B  // board update or something, switch statement will send it into the board update function or something similar
+s: A  // Acknowledge
+c: 1 3 7
+s: A
+*/
+}

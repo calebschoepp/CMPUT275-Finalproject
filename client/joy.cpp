67b134c62d88bc59b36a4bc07479dad3b51a4df2
caleb <caleb.schoepp@gmail.com>
2019-03-26 14:53:21 -0600

    Fixing the merge madness

--- client/joy.cpp
+++ client/joy.cpp
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-#include "joy.h"
-
-#include "consts_and_types.h"
-
-bool Joy::joyPressed() {
-    int read = digitalRead(JOY_SEL);
-    if (read == HIGH) {
-        return false;
-    }
-    return true;
-}
-
-direction Joy::joyMoved() {
-    int xVal = analogRead(JOY_HORIZ);
-    int yVal = analogRead(JOY_VERT);
-
-    // x is inverted
-    if (xVal - JOY_DEADZONE - JOY_CENTER > 0) {
-        return direction::LEFT;
-    } else if((1024 - xVal) - JOY_DEADZONE - JOY_CENTER > 0) {
-        return direction::RIGHT;
-    } else if (yVal - JOY_DEADZONE - JOY_CENTER > 0) {
-        return direction::DOWN;
-    } else if ((1024 - yVal) - JOY_DEADZONE - JOY_CENTER > 0) {
-        return direction::UP;
-    }
-
-    return direction::NONE;
-=======
 #include "joy.h"
 
 #include "consts_and_types.h"
@@ -56,5 +26,4 @@ direction Joy::joyMoved() {
     }
 
     return direction::NONE_D;
->>>>>>> fc824c107a88ba075ecf1ad4e7ea8399146d2d04
 }
--- client/touch.cpp
+++ client/touch.cpp
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-#include "touch.h"
-
-using namespace renderconsts;
-
-button Touch::readButtons() {
-    TSPoint touch = ts.getPoint();
-
-    // If the touch is too soft not button registered
-    if (touch.z < touchconsts::MINPRESSURE ||
-        touch.z > touchconsts::MAXPRESSURE) {
-        return button::NONE;
-    }
-
-    // Convert x and y to on screen coords
-    int16_t x = map(touch.y, touchconsts::TS_MINY,
-                       touchconsts::TS_MAXY,
-                       displayconsts::tft_width - 1,
-                       0);
-    int16_t y = map(touch.x, touchconsts::TS_MINX,
-                       touchconsts::TS_MAXX,
-                       displayconsts::tft_height - 1,
-                       0);     
-
-    if (x > button_start_x) {
-        if (y > 0 && y < button_height + border_pad) {
-            return button::BOTTOM;
-        } else if (y > button_height + border_pad && y < 2 * button_height + 2 * border_pad) {
-            return button::MIDDLE;
-        } else if (y > 2 * button_height + 2 * border_pad) {
-            return button::TOP;
-        }
-    }
-    return button::NONE;
-=======
 #include "touch.h"
 
 using namespace renderconsts;
@@ -66,5 +31,4 @@ button Touch::readButtons() {
         }
     }
     return NONE_B;
->>>>>>> fc824c107a88ba075ecf1ad4e7ea8399146d2d04
 }

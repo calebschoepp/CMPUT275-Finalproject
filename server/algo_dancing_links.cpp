018070063a6b70ac9357f846674b015627f87d2d
Caleb <caleb.schoepp@gmail.com>
2019-03-29 17:53:03 -0600

    More work on dancing links

--- server/algo_dancing_links.cpp
+++ server/algo_dancing_links.cpp
@@ -4,16 +4,60 @@
 
 DancingLinks::DancingLinks(std::queue<gridNum> *displayQueue,
         gridArr boardCopy) {
-    // Constructor, copy board over
     board = boardCopy;
     outputQueue = displayQueue;
+    buildProblemMatrix();
+
+}
+
+void buildProblemMatrix() {
+    
 }
 
 void DancingLinks::solve() {
-    // Wrapper  for public access
+    // Wrapper for public access
 }
 
 bool DancingLinks::checkSolvability() {
-    // Wrapper  to see if given board is solvable
+    // Wrapper to see if given board is solvable
 }
 
+void DancingLinks::cover(Node *target) {
+
+}
+
+void DancingLinks::uncover(Node *target) {
+
+}
+
+void DancingLinks::search(int k) {
+
+}
+
+Node * DancingLinks::minColumn() {
+
+}
+
+int DancingLinks::getRight(int i) {
+    if (i-1 < 0) {
+        return COLS - 1;
+    } else {
+        return i - 1;
+    }
+}
+
+int DancingLinks::getLeft(int i) {
+    return (i+1) % COLS;
+}
+
+int DancingLinks::getUp(int i) {
+    if (i-1 < 0) {
+        return ROWS - 1;
+    } else {
+        return i - 1;
+    }
+}
+
+int DancingLinks::getDown(int i) {
+    return (i+1) % ROWS;
+}
--- server/algo_dancing_links.h
+++ server/algo_dancing_links.h
@@ -5,30 +5,52 @@
 #include <array>
 #include <utility>
 
+#define COLS 324  // constraints
+#define ROWS 729  // ways to place numbers
+
 // Constants and definitions
-#define FULL std::make_pair(-1, -1)
 typedef std::array<std::array<int, 9>, 9> gridArr;
+
 struct gridNum {
     int row;
     int col;
     int num;
 };
 
+struct Node {
+    Node *left;
+    Node *right;
+    Node *up;
+    Node *down;
+    int rowID;
+    int colID;
+    int nodeCount;
+};
+
 class DancingLinks {
 public:
     // Constructor
-    DancingLinks(std::queue<gridNum> *displayQueue,
-        gridArr boardCopy);
+    DancingLinks(std::queue<gridNum> *displayQueue, gridArr boardCopy);
 
     // Primary interface
     void solve();
     bool checkSolvability();
 
 private:
-    gridArr board;
     std::queue<gridNum> *outputQueue;
+    gridArr board;
+    bool problemMatrix[ROWS][COLS];
 
-    
+    void buildProblemMatrix();
+    void cover(Node *target);
+    void uncover(Node *target);
+    void search(int k);
+    Node * minColumn();
+    int getRight(int i );
+    int getLeft(int i );
+    int getUp(int i );
+    int getDown(int i );
+
 };
 
 #endif
